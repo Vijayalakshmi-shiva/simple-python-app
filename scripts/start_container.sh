dc5d4d1d5996416b74debee5a696a1f93d655458
Vijayalakshmi-shiva <72096761+Vijayalakshmi-shiva@users.noreply.github.com>
2026-01-30 17:06:55 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,6 +5,6 @@ set -e
 docker pull abhishekf5/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 8000:8000 abhishekf5/simple-python-flask-app
+docker run -d -p 8006:8006 abhishekf5/simple-python-flask-app
 
 echo "***********AWS CICD successfully completed!!!*************"
